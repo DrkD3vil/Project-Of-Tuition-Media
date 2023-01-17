121ee1c246cb17c4e6963ec81564c33a95123b7f
BIJOY DEY <96878348+DrkD3vil@users.noreply.github.com>
2023-01-17 21:22:15 +0600

    react setup

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const dbConfig =require("./config/dbConfig");
-const port = process.env.PORT || 5001;
-app.use(express.json());
-app.listen(port, () => console.log(`Node Server Started at ${port}`));
