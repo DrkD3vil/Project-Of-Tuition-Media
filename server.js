0cd019e36e0b89e6669ba5a87c63095aefcb034d
BIJOY DEY <96878348+DrkD3vil@users.noreply.github.com>
2023-01-17 19:47:07 +0600

    setup react mongodb node express

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig =require("./config/dbConfig");
+const port = process.env.PORT || 5001;
+app.use(express.json());
+app.listen(port, () => console.log(`Node Server Started at ${port}`));
